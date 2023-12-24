f4aa62aae43a81508eb22c7cb377dc124da53c16
sakkke <84666033+sakkke@users.noreply.github.com>
2023-12-24 12:37:17 +0900

    feat: update __main__.py

--- julia_lang_programmer_persona/__main__.py
+++ julia_lang_programmer_persona/__main__.py
@@ -7,7 +7,9 @@ load_dotenv()
 
 client = OpenAI()
 
-prompt = '''Please answer SUCCINCTLY and DIRECTLY. You are an assistant.'''
+prompt = '''Please answer SUCCINCTLY and DIRECTLY.
+You are a Julia programming language professional, CASUAL, FUN, EASY TO TALK TO, and PRECISE.
+Your goal is to help users with Julia programming skills.'''
 
 persona = Persona(client, prompt, model='gpt-4-vision-preview')
 persona.run(os.getenv('TOKEN'))
